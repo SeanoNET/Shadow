0133eab4c67ea39e60de02b747a79aa82fe94e04
Sean O <27134812+SeanoNET@users.noreply.github.com>
2021-06-10 16:46:24 +0800

    init

---
+++ src/Shadow/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace Shadow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using var watcher = new FileSystemWatcher(@"C:\Users\seanp\source\repos\Shadow");
+            watcher.Changed += OnChanged;
+            watcher.Filter = "default.log";
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+
+            //Read file at the start
+            var fs = new FileStream(@"C:\Users\seanp\source\repos\Shadow\default.log", FileMode.Open, FileAccess.Read, FileShare.ReadWrite);          
+            using (fs)
+            {
+                var b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+                    Console.WriteLine(temp.GetString(b));
+                }
+                totalReadBytes = fs.Length;
+            }
+           
+            Console.ReadLine();         
+        }
+        static long totalReadBytes = 0;
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType != WatcherChangeTypes.Changed)
+            {
+                return;
+            }
+
+            var fs = new FileStream(@"C:\Users\seanp\source\repos\Shadow\default.log", FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            
+            using (fs)
+            {
+                var b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+                while (fs.Read(b, (int)totalReadBytes, (int)(fs.Length-totalReadBytes)) > 0)
+                {
+                    Console.WriteLine(temp.GetString(b));
+                }
+                totalReadBytes = fs.Length;
+            }
+            Console.WriteLine($"Changed: {e.FullPath}");
+            Console.WriteLine($"Buffer Size: {totalReadBytes.ToString()}");
+        }
+    }
+}
